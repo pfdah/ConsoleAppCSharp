e210550922527d63e241db5b6455ee9fdae940b3
pfdah <pratikdahal700@gmail.com>
2020-09-21 11:29:08 +0545

    Basics of Class

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AcademiaStuff
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.Write("\nEnter Something Awesome\n");
+            string test = Console.ReadLine();
+            Console.Write("\n\n");
+            Student x = new Student(test);
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademiaStuff
+{
+    public class Student
+    {
+        string test = "";
+        public Student()
+        {
+            test = "";
+            Console.WriteLine("This is Empty Class");
+            Console.Write("-_-\n");
+        }
+        public Student(string Textt)
+        {
+            test = Textt;
+            Console.WriteLine("This is non-Empty Class");
+            Console.WriteLine(test);
+            Console.WriteLine("o_o");
+        }
+    }
+}
